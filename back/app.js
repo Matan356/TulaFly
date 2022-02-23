350be4bf75d2d21275c323669e400068f12e527b
Matan356 <79919211+Matan356@users.noreply.github.com>
2022-02-23 10:02:28 +0200

    set mongoose commection & bodyparser & cors

--- back/app.js
+++ back/app.js
@@ -1,5 +1,21 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
 
-app.listen(5000);
+app.use(cors());
+app.use(bodyParser.json());
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.qk4ej.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    app.listen(process.env.PORT || 5000);
+    console.log("connect to database!");
+  })
+  .catch((err) => {
+    console.log("connection faild!");
+    console.log(err);
+  });
