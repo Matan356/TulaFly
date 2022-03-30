80ea57840ba5cccbdff9b77e234e1fb59ec031c8
Matan356 <79919211+Matan356@users.noreply.github.com>
2022-03-30 20:27:52 +0300

    added loading spiner

---
+++ front/src/shared/components/UIElements/LoadingSpiner.js
@@ -0,0 +1,10 @@
+import { CircularProgress } from '@mui/material'
+import React from 'react'
+
+const LoadingSpiner = () => {
+  return (
+    <div> <CircularProgress color="inherit" /></div>
+  )
+}
+
+export default LoadingSpiner
