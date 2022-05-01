32cb2abe7c71f943227d74de40eec3ae4ba6fa14
Matan356 <79919211+Matan356@users.noreply.github.com>
2022-05-01 13:35:23 +0300

    Completion of data matching to the admin dashboard

--- front/src/admin/pages/DashBoard.js
+++ front/src/admin/pages/DashBoard.js
@@ -6,14 +6,13 @@ import {
   CssBaseline,
   Grid,
   Link,
-  Paper,
   Toolbar,
   Typography,
   Container,
 } from "@mui/material";
 
-import Chart from "../components/Chart";
-import Orders from "../components/Orders";
+// import Orders from "../components/Orders";
+import ChartCom from "../components/ChartCom";
 
 function Copyright(props) {
   return (
@@ -24,26 +23,23 @@ function Copyright(props) {
       {...props}
     >
       {"Copyright © "}
-      <Link color="inherit" href="https://mui.com/">
+      <Link color="inherit" href="http://localhost:3000/">
         Your Website
       </Link>{" "}
       {new Date().getFullYear()}
       {"."}
     </Typography>
-  ); 
+  );
 }
-
 
 const mdTheme = createTheme();
 
 function DashboardContent() {
-
-
   return (
     <ThemeProvider theme={mdTheme}>
       <Box sx={{ display: "flex" }}>
         <CssBaseline />
- 
+
         <Box
           component="main"
           sx={{
@@ -57,39 +53,10 @@ function DashboardContent() {
           }}
         >
           <Toolbar />
-          <Container maxWidth="lg" sx={{ mt: {xl:4,xs:13}, mb: 4 }}>
+          <Container maxWidth="xl" sx={{ mt: { xl: 4, xs: 13 }, mb: 4 }}>
             <Grid container spacing={3}>
-              {/* Chart */}
-              <Grid item xs={12} md={8} lg={9}>
-                <Paper
-                  sx={{
-                    p: 2,
-                    display: "flex",
-                    flexDirection: "column",
-                    height: 240,
-                  }}
-                >
-                  <Chart />
-                </Paper>
-              </Grid>
-              {/* Recent Deposits */}
-              {/* <Grid item xs={12} md={4} lg={3}>
-                  <Paper
-                    sx={{
-                      p: 2,
-                      display: 'flex',
-                      flexDirection: 'column',
-                      height: 240,
-                    }}
-                  >
-                    <Deposits />
-                  </Paper>
-                </Grid> */}
-              {/* Recent Orders */}
-              <Grid item xs={12}>
-                <Paper sx={{ p: 2, display: "flex", flexDirection: "column" }}>
-                  <Orders />
-                </Paper>
+              <Grid item xs={12} md={12} lg={12}>
+                <ChartCom />
               </Grid>
             </Grid>
             <Copyright sx={{ pt: 4 }} />
