2a6193ececdd4d3a715ce46b718a5b723a831378
Matan356 <79919211+Matan356@users.noreply.github.com>
2022-05-04 21:05:29 +0300

    adding socket.io connection

--- back/app.js
+++ back/app.js
@@ -7,25 +7,58 @@ const usersRoutes = require("./routes/users-routes");
 const vacationsRoutes = require("./routes/vacations-routes");
 const cartRoutes = require("./routes/cart-routes");
 const adminRoutes = require("./routes/admin-routes");
+const socket = require("socket.io");
 
 const app = express();
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use(cors());
+app.use(bodyParser.json({ limit: "50mb" })); 
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+app.use(express.json());
+
 app.use("/main", usersRoutes);
 app.use("/vacations", vacationsRoutes);
 app.use("/cart", cartRoutes);
-app.use("/panelAdmin", adminRoutes); 
- 
-mongoose 
+app.use("/panelAdmin", adminRoutes);
+
+mongoose
   .connect(
-    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.qk4ej.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.qk4ej.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
   )
   .then(() => {
-    app.listen(process.env.PORT || 5000);
     console.log("connect to database!");
   })
   .catch((err) => {
     console.log("connection faild!");
-    console.log(err);
+    console.log(err.massage);
   });
+// Set up socket.io
+
+const port = process.env.PORT || 5000;
+
+const server = app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
+
+const io = socket(server, {
+
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PATCH"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("socket connection succesful");
+  socket.on("adminUpdate", (res) => {
+    if (res) {
+      //Change the name from create Vactaion to adminUpdate
+      //if res is true pass the new array of vacations
+      console.log(res);
+      socket.emit("updateUserVacations", res);
+    }
+  });
+});
